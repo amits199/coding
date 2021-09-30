777dad8c3b10c091175718c02265d990808668e3
Amit RaJ <72591255+amitraj991@users.noreply.github.com>
2021-10-01 00:56:55 +0530

    Update checkprime.py

--- checkprime.py
+++ checkprime.py
@@ -4,10 +4,14 @@ n=int(input("ente the number"))
 for i in range(1,n+1):
     if n%i==0:
         factors=factors+[i]
+     
 
 if len(factors==2):
-    print("yes prime")
+    print("yes the number is prime")
+    
+else:
+    print("wrong input")
 
 else:
-    print("not prime")
+    print("no number is not prime prime")
 
